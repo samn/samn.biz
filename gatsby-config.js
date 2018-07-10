839e72fdb633e51383f3331b18e32c73e0eb225e
Sam Neubardt <samneubardt@gmail.com>
2018-07-10 11:14:26 -0400

    Add typography plugin & config
    replacing the default css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Sam Neubardt: Professional Bad Boy 😎',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      },
+    },
+  ],
 }
