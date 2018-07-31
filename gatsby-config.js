ed33f32d8a950fcc4033f84de00b9784b461e1e9
Sam Neubardt <samneubardt@gmail.com>
2018-07-31 17:37:32 -0400

    Add gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
+    'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
