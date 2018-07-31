2a72b6b3d0480b6893a480167f52a1a0970a2427
Sam Neubardt <samneubardt@gmail.com>
2018-07-31 16:44:50 -0400

    Fix footer layout, now always pushed to the bottom
    it was breaking with longer content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,22 +8,20 @@ const containerStyle = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
-  height: '100%',
-  width: '100%',
-  position: 'absolute',
 };
 
-const topContainerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'flex-start',
+const contentContainerStyle = {
+  margin: '0 auto',
+  maxWidth: 960,
+  padding: '0px 1.0875rem 1.45rem',
+  paddingTop: 0,
 };
 
 const footerContainerStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  marginBottom: 10,
+  margin: 10,
 };
 
 const Layout = ({ children, data }) => (
@@ -35,16 +33,9 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'neubardt, recipes, programming' },
       ]}
     />
-    <div style={topContainerStyle}>
+    <div>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
+      <div style={contentContainerStyle}>
         {children()}
       </div>
     </div>
